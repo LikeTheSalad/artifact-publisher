5abe6d539ab8c8aa661173f2344bd98c2e3b3be3
Cesar Munoz <likethesalad@gmail.com>
2025-07-26 11:38:40 +0200

    Migrating to kts

---
+++ build.gradle.kts
@@ -0,0 +1,116 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
+plugins {
+    id("java-gradle-plugin")
+    id("org.jetbrains.kotlin.jvm") version "2.2.0"
+    id("maven-publish")
+    id("signing")
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("com.gradle.plugin-publish") version "1.2.1"
+}
+
+description = "Internal plugin for publishing LikeTheSalad Java and Android libs"
+
+val publishingCoordinates = Coordinates(
+    "Artifact Publisher",
+    "https://github.com/LikeTheSalad/artifact-publisher",
+    "https://github.com/LikeTheSalad/artifact-publisher.git",
+    "https://github.com/LikeTheSalad/artifact-publisher/issues"
+)
+
+data class Coordinates(
+    val publicName: String,
+    val url: String,
+    val vcsUrl: String,
+    val issueTrackerUrl: String
+)
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    google()
+}
+
+val javaVersion = JavaVersion.VERSION_11
+val minKotlinVersion = KotlinVersion.KOTLIN_1_9
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.fromTarget(javaVersion.toString()))
+        apiVersion = minKotlinVersion
+        languageVersion = minKotlinVersion
+    }
+}
+
+dependencies {
+    api("org.jetbrains.dokka:dokka-gradle-plugin:1.8.10")
+    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
+    implementation("com.gradle.publish:plugin-publish-plugin:1.2.1")
+    implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
+}
+
+gradlePlugin {
+    website.set(publishingCoordinates.url)
+    vcsUrl.set(vcsUrl)
+    plugins {
+        create("publisher") {
+            id = "com.likethesalad.artifact-publisher"
+            displayName = publishingCoordinates.publicName
+            description = project.description
+            tags.addAll(listOf("publisher", "likethesalad"))
+            implementationClass = "com.likethesalad.tools.artifact.publisher.ArtifactPublisherPlugin"
+        }
+    }
+}
+
+java {
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    withJavadocJar()
+    withSourcesJar()
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
+
+publishing {
+    publications {
+        create("likethesalad", MavenPublication::class.java) {
+            from(components.getByName("java"))
+
+            pom {
+                name = publishingCoordinates.publicName
+                description = project.description
+                url = publishingCoordinates.url
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "LikeTheSalad"
+                        name = "Cesar Munoz"
+                        email = "likethesalad@gmail.com"
+                    }
+                }
+                scm {
+                    url = publishingCoordinates.url
+                    connection = publishingCoordinates.vcsUrl
+                }
+                issueManagement {
+                    url = publishingCoordinates.issueTrackerUrl
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications.getByName("likethesalad"))
+}
